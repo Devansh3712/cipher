1ef163ff2342303b07c82c30c6f058e535995823
Devansh3712 <devanshamity@gmail.com>
2022-03-09 22:14:26 +0530

    Update `test_ascii.cpp`

--- tests/test_ascii.cpp
+++ tests/test_ascii.cpp
@@ -2,10 +2,17 @@
 #include <cassert>
 #include <string>
 #include "../lib/ascii.hpp"
+#include "../lib/fileio.hpp"
 
 int main(){
     std::string str = "test data", result = "116 101 115 116 32 100 97 116 97";
-    ASCIICode obj(str);
-    assert(obj.encrypt() == result);
-    assert(obj.decrypt() == str);
+    ASCIICode obj1(str), obj2("./tests/test_file.txt", true);
+    assert(obj1.encrypt() == result);
+    assert(obj1.decrypt() == str);
+    obj2.encrypt();
+    FileIO encrypted("./tests/test_file.txt");
+    assert(encrypted.read() == result);
+    obj2.decrypt();
+    FileIO decrypted("./tests/test_file.txt");
+    assert(decrypted.read() == str);
 }
